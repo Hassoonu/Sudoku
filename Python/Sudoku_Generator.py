6be1579241f5f8787ebddce11c5071ed9cc22112
Hasan Al Saeedi <Hasan2002618@gmail.com>
2025-11-22 01:15:33 -0600

    fixed encoding, added Python folder

---
+++ Python/Sudoku_Generator.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+EMPTY_CELL = -1
+
+def check_valid(puzzle: np.ndarray[tuple[int, int], np.dtype[np.uint8]]) -> bool:
+    
+
+def generate_puzzle(filled_in: uint8) -> np.ndarray[tuple[int, int], np.dtype[np.uint8]]:
+    pass
